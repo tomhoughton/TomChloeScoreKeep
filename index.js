7a768649fd9bc697b0e25b35ebbbfed28f473dca
Thomas Houghton <tomhoughton888@gmail.com>
2022-07-31 14:18:25 +0100

    Changed MongoDB env variable

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post('/api/add-player', (req, res) => {
 app.listen(process.env.PORT, () => console.log(`Server is running on port: ${PORT} mongo: ${process.env.MONGODB} hello world: ${process.env.HelloWorld}`));
 
 // Connect to MongoDB:
-mongoose.connect(process.env.MONGODB, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
     .then(() => {
         console.log('MongoDB Successful');
     });
