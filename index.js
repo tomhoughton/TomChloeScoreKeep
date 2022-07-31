712b709cd8d2f0b1d34940ea411ef9fbd25aad84
Thomas Houghton <tomhoughton888@gmail.com>
2022-07-31 14:48:16 +0100

    Update

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ const createNewGame = (players, name) => {
 // TODO: Possible get game to ID, or player to ID Api Routes.
 // This will be for the default page:
 app.use(express.static(path.join(__dirname, '../build')))
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin");
     res.sendFile(path.join(__dirname, '../client/buildindex.html'));
 });
 
